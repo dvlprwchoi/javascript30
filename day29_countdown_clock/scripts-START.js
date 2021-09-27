1337facbf197d7d1deac41b169fa4fa547d37afb
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-09-26 23:40:11 -0500

    Display end time function

--- day29_countdown_clock/scripts-START.js
+++ day29_countdown_clock/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
   //   console.log({ now, then });
   setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -30,4 +32,14 @@ function displayTimeLeft(seconds) {
 
   document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+
+  endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
 }
